cf6993c60588d3f33bfce9ce9af261b3d148d593
JohnsonNyamweya <johnsonomari1032@gmail.com>
2022-06-05 13:37:19 +0300

    Added sellers_list_details and deliverers_list_details layouts

--- app/src/main/java/com/johnsonnyamweya/businesspro/Admins/DeliverersListActivity.java
+++ app/src/main/java/com/johnsonnyamweya/businesspro/Admins/DeliverersListActivity.java
@@ -3,14 +3,22 @@ package com.johnsonnyamweya.businesspro.Admins;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.johnsonnyamweya.businesspro.R;
 
 public class DeliverersListActivity extends AppCompatActivity {
 
+    private RecyclerView deliverersListRv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deliverers_list);
+
+        deliverersListRv = (RecyclerView) findViewById(R.id.deliverers_list_rv);
+        deliverersListRv.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
--- app/src/main/java/com/johnsonnyamweya/businesspro/Admins/SellersListActivity.java
+++ app/src/main/java/com/johnsonnyamweya/businesspro/Admins/SellersListActivity.java
@@ -3,14 +3,22 @@ package com.johnsonnyamweya.businesspro.Admins;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.johnsonnyamweya.businesspro.R;
 
 public class SellersListActivity extends AppCompatActivity {
 
+    private RecyclerView sellersRv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sellers_list);
+
+        sellersRv = (RecyclerView) findViewById(R.id.sellers_list_rv);
+        sellersRv.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
